61c94646e2a8190265c314ca749dbaad28988dcb
300960367 <fito@my.centennialcollege.ca>
2018-03-12 19:43:19 -0400

    "Starting the Photorama Application" completed.

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by FERNANDO ITO on 2018-03-12.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+class PhotoViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
+
