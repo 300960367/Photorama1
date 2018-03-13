4edc05c789b3c25d4fe6d8c92a20eb39ac61884d
300960367 <fito@my.centennialcollege.ca>
2018-03-12 21:58:16 -0400

    "The Main Thread" completed

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -20,6 +20,9 @@ class PhotosViewController: UIViewController {
             switch photosResult {
             case let .success(photos):
                 print ("Successfully found \(photos.count) photos.")
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
             case let .failure(error):
                 print ("Error fetching interesting photos: \(error)")
             }
@@ -28,6 +31,18 @@ class PhotosViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
     var store: PhotoStore!
-
+    
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
+    } // func
 }
 
